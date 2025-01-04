2baa370cdf4fb24b333c126f2ebe9775408514a1
David <45155292+DavoDC@users.noreply.github.com>
2025-01-04 13:39:10 +0800

    Improve Music script
    - Can handle both VLC installation paths, and gives message
    - Better conditional organisation - final command at bottom after checks
    - Added explanation of VLC options

--- Scripts/Batch/bat_play_All_Music.bat
+++ Scripts/Batch/bat_play_All_Music.bat
@@ -1,17 +1,42 @@
 @echo off
 
-set "fpath=C:\Users\%username%\Audio"
-@REM set "vlcpath=C:\Program Files\VideoLAN\VLC\vlc.exe"
-set "vlcpath=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+set "audioPath=C:\Users\%username%\Audio"
+set "vlcpath1=C:\Program Files\VideoLAN\VLC\vlc.exe"
+set "vlcpath2=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+set "vlcpath="
 
-dir /b /s "%fpath%\*.mp3" >nul 2>&1
-if not errorlevel 1 (
-    start "" "%vlcpath%" "%fpath%" --playlist-autostart --no-playlist-tree --auto-preparse --random --loop -Z
-    exit
+:: Find VLC installation path
+if exist "%vlcpath1%" (
+    set "vlcpath=%vlcpath1%"
+) else if exist "%vlcpath2%" (
+    set "vlcpath=%vlcpath2%"
 ) else (
-    echo Audio folder is empty or non-existent!
+    echo.
+    echo "VLC is not installed or cannot be found!"
+    echo.
     pause
+    exit /b
 )
 
-@REM To fix reading of non-audio file extensions, add the ext to:
-@REM Tools -> Prefs -> All -> Playlist -> Ignored extensions
+:: Check if audio folder contains MP3 files
+dir /b /s "%audioPath%\*.mp3" >nul 2>&1
+if errorlevel 1 (
+    echo.
+    echo "Audio folder is empty or non-existent!"
+    echo.
+    pause
+    exit /b
+)
+
+:: Launch VLC with specified options
+:: --no-playlist-tree - Disables hierarchical grouping and expands all items into a flat list.
+:: --playlist-autostart - Automatically starts playing the playlist.
+:: --auto-preparse - Pre-parses metadata for media files in the playlist before they are played.
+:: --random - Shuffles the playlist.
+:: --loop - Loops the playlist continuously.
+:: -Z - Enables random and loop together (shuffled loop).
+start "" "%vlcpath%" "%audioPath%" --no-playlist-tree --playlist-autostart --auto-preparse --random --loop -Z
+exit /b
+
+:: To fix reading of non-audio file extensions, add the ext to:
+:: Tools -> Prefs -> All -> Playlist -> Ignored extensions
